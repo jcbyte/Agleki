fefd2397844be82a979cbfd42a11e6391aa1d76f
jcbyte <57547887+jcbyte@users.noreply.github.com>
2023-05-09 21:51:40 +0100

    js movement

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { IconButton, LinearProgress } from "@mui/material";
+import { Card, CardActionArea, CardContent, IconButton, LinearProgress } from "@mui/material";
 import CheckIcon from "@mui/icons-material/Check";
 import ClearIcon from "@mui/icons-material/Clear";
-
-import FlippableCard from "./FlippableCard";
+const Latex = require("react-latex");
 
 export default function App() {
+	const [showingFront, setShowingFront] = useState(true);
 	const [cardContent, setCardContent] = useState({ front: "", back: "" });
-	const [resetCard_FLAG, setResetCard_FLAG] = useState(0);
 
 	const [difficulty, setDifficulty] = useState(0);
 
 	function getWeightedDifficulty() {
-		return difficulty / (difficulty + 1);
+		return difficulty / (difficulty + 4);
 	}
 
 	function getNewQuestion(prevSuccess) {
@@ -20,7 +19,7 @@ export default function App() {
 			.then((res) => res.json())
 			.then((data) => {
 				setCardContent({ front: `$${data.question}$`, back: `$${data.answer}$` });
-				setResetCard_FLAG((prev) => prev + 1);
+				setShowingFront(true);
 			});
 	}
 
@@ -30,7 +29,18 @@ export default function App() {
 
 	return (
 		<>
-			<FlippableCard frontContent={cardContent.front} backContent={cardContent.back} resetFront_FLAG={resetCard_FLAG} />
+			<Card variant="outlined" sx={{ minWidth: 275 }} style={{ margin: 10 }}>
+				<CardActionArea
+					onClick={() => {
+						setShowingFront((prev) => !prev);
+					}}
+				>
+					<CardContent>
+						<Latex>{showingFront ? cardContent.front : cardContent.back}</Latex>
+					</CardContent>
+				</CardActionArea>
+			</Card>
+
 			<IconButton
 				onClick={() => {
 					setDifficulty((prev) => Math.max(prev - 1, 0));
@@ -47,6 +57,7 @@ export default function App() {
 			>
 				<CheckIcon />
 			</IconButton>
+
 			<LinearProgress variant="determinate" value={getWeightedDifficulty() * 100} />
 		</>
 	);
