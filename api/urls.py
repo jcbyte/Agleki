147a525004ccd8c32198e5f5b6c530246fbe93cd
jcbyte <57547887+jcbyte@users.noreply.github.com>
2023-05-09 18:59:42 +0100

    api test

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
 
 urlpatterns = []
+
+from .views import (
+    GetQuestion,
+)
+
+urlpatterns = [
+    path("getQuestion", GetQuestion.as_view()),
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import JsonResponse
+
 # Create your views here.
+
+
+class GetQuestion(APIView):
+    def get(self, req, format=None):
+        data = {"test": 200}
+        return JsonResponse(data, status=status.HTTP_200_OK)
