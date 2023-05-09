5c44517e6789207d8435065ff69c8c69ad886513
Alecfore <57565395+Alecfore@users.noreply.github.com>
2023-05-09 20:40:41 +0100

    backendbrokenaf

--- api/views.py
+++ api/views.py
@@ -12,24 +12,46 @@ import random
 
 
 class GetQuestion(APIView):
-    def LinExpr2Range(self, symbol, nRange, sRange, symbolChance):
-        if (symbolChance > random.random()):
-            return Symbol(symbol) * random.randint(sRange[0], sRange[1]) + random.randint(nRange[0], nRange[1])
-        else:
-            return random.randint(nRange[0], nRange[1])
+    def LinExpr(self, symbol, nRange, symbolChance):
+        values = [num for num in range(nRange[0], nRange[1]+1) if num!=0]
         
-    def LinExpr(self, symbol, range, symbolChance):
         if (symbolChance > random.random()):
-            return Symbol(symbol) * random.randint(range[0], range[1]) + random.randint(range[0], range[1])
+            return Symbol(symbol) * random.choice(values) + random.choice(values)
         else:
-            return random.randint(range[0], range[1])
+            return random.choice(values)
+        
+    def LinExprSymbols(self, symbol1, symbol2, nRange):
+        values = [num for num in range(nRange[0], nRange[1]+1) if num!=0]
+    
+        return Symbol(symbol1) * random.choice(values) + Symbol(symbol2) * random.choice(values)
+
+    def SingleExprSymbol(self, symbol, nRange, expRange):
+        values = [num for num in range(nRange[0], nRange[1]+1) if num!=0]
+        return (Symbol(symbol) ** random.choice(expRange)) * random.choice(nRange)
+
+    def QDeterminant(self):
+        M = Matrix([
+            [self.LinExpr('k', [-3, 3], 0.5), self.LinExpr('k', [-3, 3], 0.5)], 
+            [self.LinExprSymbols('k', 'a', [-3, 3]), self.LinExpr('k', [-3, 3], 0.5)]
+            ])
+        return {"Question" : latex(M) , "Answer" : latex(M.det())}
+
+    def QExpand(self):
+        expr1 = self.LinExprSymbols('a', 'b', [-3, 3])
+        expr2 = self.LinExprSymbols('a', 'b', [-3, 3])
+        return {"Question" : latex(expr1 * expr2) , "Answer" : latex(expand(expr1 * expr2))}
+
+    def QSimplify(self):
+        expr = self.SingleExprSymbol('q', [-3, 3], [1, 3]) * (
+            self.LinExpr('p', [-4, 4], 0.99) + self.LinExprSymbols('p', 'pi', [-4, 4])
+        ) + self.SingleExprSymbol('q', [-3, 3], [1, 3]) * self.LinExpr('p', [-4, 4], 0.99)
+        return {"Question" : expr , "Answer" : latex(expand(expr))}
+        
 
     def get(self, req, format=None):
         x = Symbol('x')
-        M = Matrix([
-            [self.LinExpr('k', [-3, 3], 0.5), self.LinExpr('k', [-3, 3], 0.5)], 
-            [self.LinExpr('k', [-3, 3], 0.5), self.LinExpr('k', [-3, 3], 0.5)]
-            ])
+        
 
-        data = {"Question" : latex(M) , "Answer" : latex(M.det())}
+        #data = {"Question" : latex(M) , "Answer" : latex(M.det())}
+        data = self.QSimplify()
         return JsonResponse(data, status=status.HTTP_200_OK)
